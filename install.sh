9fbf9dd1e0161316ba309ac4baae57902ea7e8b3
deciding <zhangzn710@gmail.com>
2019-01-17 15:23:22 +0000

    zz

--- install.sh
+++ install.sh
@@ -14,6 +14,26 @@ sudo pip3 install virtualenv
 mkdir ~/.virtualenvs
 sudo pip3 install virtualenvwrapper
 
-cp .bashtc ~/,bashrc
+# install Vundle and install all the specified plugins
+echo "Installing Vundle" 
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+cp .vimrc ~/.vimrc
+vim +PluginInstall +qall
+#install ctags
+echo "Installing ctags"
+sudo apt-get install exuberant-ctags
+echo "Installing taglist"
+#install taglist
+wget -O /usr/src/taglist.zip http://www.vim.org/scripts/download_script.php?src_id=7701
+unzip /usr/src/taglist.zip -d $HOME/.vim # with 'filetype plugin on' in ~/.vimrc
+#install YCM, not sure whether Vundle install only will work
+echo "Installing YCM"
+sudo apt-get install build-essential cmake
+sudo apt-get install python-dev python3-dev
+git clone https://github.com/Valloric/YouCompleteMe.git $HOME/.vim/bundle/YouCompleteMe
+(cd $HOME/.vim/bundle/YouCompleteMe && git submodule update --init --recursive && ./install.py --clang-completer)
+#install ConqueGDB
+
+cp .bashtc ~/.bashrc
 source ~/.bashrc
 
