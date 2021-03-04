9ffdb2e3b76c5f32441acd9d2891a9a7597f7704
deciding <zhangzn710@gmail.com>
2021-03-04 14:28:40 +0000

    vimrc for c++; cmake latest install

--- install.sh
+++ install.sh
@@ -140,3 +140,9 @@ rm -rf TerminalImageViewer
 #        --with-python3-config-dir=/usr/lib/python3.6/config-*
 #    make
 #    sudo make install
+# 5. cmake latest
+#    sudo apt purge --auto-remove cmake
+#    wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+#    sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'     #focal 20.04, bionic 18.04, xenial 16.04
+#    sudo apt update
+#    sudo apt install cmake
